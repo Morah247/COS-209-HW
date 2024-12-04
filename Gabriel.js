d9e3e72c2af51ef53a08cc0dff449c26ca041227
Morah247 <morah.tochukwu@lmu.edu.ng>
2024-12-04 14:21:46 +0100

    Add files via upload

---
+++ Gabriel.js
@@ -0,0 +1,20 @@
+let student = {
+   'Name': 'MORAH TOCHUKWU GABRIEL',
+    Age: 36,
+   'Application ID': 'LT247620',
+   'Matric_No': '21/2634' ,
+    School: 'Comupting & Engineering Sciences',
+    Course: 'Software Engineering',
+   'Phone No': 9067267398 ,
+    Email: 'morahtochuckwu@gmail.com',
+    Nationality: 'Nigerian',
+    Address:  {
+       Street: '1045 Egundechemba close',
+       LGA: 'Festac',
+       State: 'Eboniyi State',
+       Country: 'Nigeria'
+      }     
+                     
+   };
+ console.log(student)
+
